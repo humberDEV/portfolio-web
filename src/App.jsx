c0200da5f79c091197d0792b8cef4313ea30e3e0
Humberto García Arellano <humberbrawl@gmail.com>
2023-08-24 23:06:12 +0200

    First version

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Navbar } from './components/navbar'
+
+export function App() {
+  return (
+    <>
+      <section>
+        <Navbar />
+      </section>
+      <main>
+        {/* el contenido de la pagina */}
+      </main>
+      <footer>
+        {/* componente footer */}
+      </footer>
+    </>
+  )
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <nav>
+            <div>
+                mi barra de navegacion
+            </div>
+        </nav>
+    )
+}
